bedd1dc53cab30faf6015739150da2c4c6624ae2
hrsh7th <hrsh7th@gmail.com>
2019-03-13 15:17:45 +0900

    Support log reset actions

--- rplugin/python3/denite/kind/gitto_log.py
+++ rplugin/python3/denite/kind/gitto_log.py
@@ -5,5 +5,20 @@ class Kind(Base):
         super().__init__(vim)
         self.name = 'gitto/log'
         self.redraw_actions = []
+        self.redraw_actions += ['reset']
+        self.redraw_actions += ['reset_soft']
+        self.redraw_actions += ['reset_hard']
         self.persist_actions = self.redraw_actions
 
+    def action_reset(self, context):
+        self._reset(context, {})
+
+    def action_reset_soft(self, context):
+        self._reset(context, {'--soft': True})
+
+    def action_reset_hard(self, context):
+        self._reset(context, {'--hard': True})
+
+    def _reset(self, context, opts):
+        commit_hash = context['targets'][0]['action__log']['commit_hash']
+        self.vim.call('gitto#run', 'log#reset', commit_hash, opts)
--- rplugin/python3/denite/source/gitto_log.py
+++ rplugin/python3/denite/source/gitto_log.py
@@ -7,10 +7,17 @@ class Source(Base):
 
         self.name = 'gitto/log'
         self.kind = 'gitto/log'
-        self.vars = {}
+        self.vars = {
+                'default_option': {
+                        '--no-merges': True
+                    }
+                }
 
     def gather_candidates(self, context):
-        logs = self.vim.call('gitto#run', 'log#get')
+        logs = self.vim.call('gitto#run', 'log#get', self.vars['default_option'], context['args'][0] if len(context['args']) > 0 else '')
+        if len(logs) <= 0:
+            return []
+
         author_name_len = max([len(x['author_name']) for x in logs])
         return [{
             'word': '{:>20} | {:<{width}} | {}'.format(log['author_date'],
