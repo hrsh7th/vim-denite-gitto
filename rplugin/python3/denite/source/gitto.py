903123b2fe7073212ce25eda036ba30a5d730f63
hrsh7th <hrsh7th@gmail.com>
2019-03-14 17:14:21 +0900

    Update gitto source

--- rplugin/python3/denite/source/gitto.py
+++ rplugin/python3/denite/source/gitto.py
@@ -19,36 +19,30 @@ class Source(Base):
         return [candidate for candidate in candidates if candidate]
 
     def _push(self, context):
-        branch = self.vim.call('gitto#run', 'branch#current')
-        if not branch:
-            return
         return {
-            'word': '{:<{label}}: {} to {}'.format('push', branch['name'], branch['remote'], label=self.label_len),
-            'abbr': '{:<{label}}: {} to {}'.format('push', branch['name'], branch['remote'], label=self.label_len),
-            'action__command': 'call gitto#do("branch#push")({})'.format(branch['name'])
+            'word': 'push',
+            'abbr': 'push',
+            'action__command': 'call gitto#do("branch#push")()'
         }
 
     def _push_force(self, context):
-        branch = self.vim.call('gitto#run', 'branch#current')
-        if not branch:
-            return
         return {
-            'word': '{:<{label}}: {} to {}'.format('force push', branch['name'], branch['remote'], label=self.label_len),
-            'abbr': '{:<{label}}: {} to {}'.format('force push', branch['name'], branch['remote'], label=self.label_len),
-            'action__command': 'call gitto#do("branch#push")({}, {{ "--force": v:true }})'.format(branch['name'])
+            'word': 'push force',
+            'abbr': 'push force',
+            'action__command': 'call gitto#do("branch#push")({ "--force": v:true })'
         }
 
     def _status(self, context):
         return {
-            'word': '{:<{label}}: show current status'.format('status', label=self.label_len),
-            'abbr': '{:<{label}}: show current status'.format('status', label=self.label_len),
+            'word': 'status',
+            'abbr': 'status',
             'action__command': 'Denite gitto/status'
         }
 
     def _log(self, context):
         return {
-            'word': '{:<{label}}: show logs'.format('log', label=self.label_len),
-            'abbr': '{:<{label}}: show logs'.format('log', label=self.label_len),
+            'word': 'log',
+            'abbr': 'log',
             'action__command': 'Denite gitto/log'
         }
 
