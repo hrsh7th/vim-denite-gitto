18d6dbbf0f7a43448589f03479ccb7c6713eff09
hrsh7th <hrsh7th@gmail.com>
2019-03-17 01:53:33 +0900

    More message

--- rplugin/python3/denite/source/gitto.py
+++ rplugin/python3/denite/source/gitto.py
@@ -31,7 +31,7 @@ class Source(Base):
     def _push(self, context, candidates, branch):
         if branch and branch['ahead'] > 0:
             candidates.append({
-                'word': ['push', 'target is `{}/{}`'.format(branch['remote'], branch['name'])],
+                'word': ['push', 'target is `{}/{}`, {} commits'.format(branch['remote'], branch['name'], branch['ahead'])],
                 'action__type': 'func',
                 'action__func': 'gitto#run',
                 'action__args': ['repo#push']
@@ -40,7 +40,7 @@ class Source(Base):
     def _push_force(self, context, candidates, branch):
         if branch and branch['ahead'] > 0:
             candidates.append({
-                'word': ['push force', 'target is `{}/{}`'.format(branch['remote'], branch['name'])],
+                'word': ['push force', 'target is `{}/{}`, {} commits'.format(branch['remote'], branch['name'], branch['ahead'])],
                 'action__type': 'func',
                 'action__func': 'gitto#run',
                 'action__args': ['repo#push', {'--force': True}]
